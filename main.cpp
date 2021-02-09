73c605b1a79353c0055c1778541e78501496a3d7
BlaiseLebreton <blaise.lebreton@sfr.fr>
2021-02-09 19:02:16 +0100

    Synthax et clean-up des variables

--- main.cpp
+++ main.cpp
@@ -13,46 +13,46 @@
 using namespace cv;
 using namespace std;
 
+// Resolution
 #define WIDTH 852
 #define HEIGHT 480
 
-
 RNG rng(12345);
 
+int   cmin    = 65;   // Minimum size of contour
+int   cmax    = 1000; // Maximum size of contour
+int   meanmin = 70;   // Minimum mean intensity of contour
+int   niter   = 2;    // Number of erosion/dilatation
+float distth  = 20;   // Distance threshold for new track
+int   sky     = 110;  // Sky start
+int   klost   = 20;   // Number of frame after a track is deleted if lost
 
-int cmin = 65, cmax = 1000; // maxi/mini longeur contour
-int meanmin = 70;
-int niter  = 2; // Number of erosion/dilatation
-float distth = 20; // Distance threshold for new track
-int sky    = 110; // Sky start
-
+// Structure of tracks
 struct track {
   vector<Point2f> p; // Position
-  Rect rect; // Bounding box
-  Point2f v; // Speed
-  Point2f a; // Acceleration
-  Point2f op; // Old position
-  Point2f ov; // Old position
-  int kfound=0; // Number of time track has been updated
-  int klost=0; // Number of time track has been updated
-  bool found=true; // Number of time track has been updated
+  Rect rect;         // Bounding box
+  Point2f v;         // Speed
+  Point2f a;         // Acceleration
+  int kfound=0;      // Number of frame track has been updated
+  int klost=0;       // Number of frame track has been lost
+  bool found=true;   // Found corresponding object at frame
   Scalar color = Scalar(rng.uniform(0,255), rng.uniform(0, 255), rng.uniform(0, 255)); // Color
 };
 vector<track> Tracks;
 
+// Structure of objects
 struct object {
   Point2f p; // Position
-  Rect rect;
+  Rect rect; // Bounding box
 };
-
 vector<object> Objects;
 
+// Distance function for data association
 float Distance(object Obj, track Trck);
 
-
+// Main thread
 int main(int argc, char* argv[]) {
 
-  int x,y;
   // Execution time
   double start,stop, dt;
 
@@ -69,17 +69,22 @@ int main(int argc, char* argv[]) {
   namedWindow("Raw", WINDOW_KEEPRATIO);
   resizeWindow("Raw", WIDTH, HEIGHT);
 
+  // Break if unable to open capture
   if (!capture.isOpened()){
     cerr << "Unable to open file" << endl;
     return 0;
   }
 
+  // Images
   Mat raw, crop, filtered, decision, display;
+
+  // Acquisition loop
   while (true) {
 
     // Frame rate
     start = getTickCount();
 
+    // Get new frame
     capture >> raw;
     if (raw.empty()) {
       break;
@@ -92,15 +97,16 @@ int main(int argc, char* argv[]) {
     Rect roi = Rect(0, sky, raw.cols, raw.rows-sky-1);
     crop = raw(roi);
 
-    // Creating display image
+    // Create display image
     cvtColor(crop, display, COLOR_BGR2GRAY);
     cvtColor(display, display, COLOR_GRAY2BGR);
 
-    // Apply blur
+    // Apply gaussian blur
     GaussianBlur(crop, filtered, Size(11, 11), 0, 0);
 
     // Update the background model
     pBackSub->apply(filtered, decision);
+
     // Erosion / Dilatation
     for (int i=0; i<niter; i++) {
       dilate(decision,decision,getStructuringElement(MORPH_ELLIPSE,Size(3,3)));
@@ -112,35 +118,44 @@ int main(int argc, char* argv[]) {
     // // Show decision
     // cvtColor(decision, display, COLOR_GRAY2BGR);
 
+    // Remove all old objects
+    Objects.clear();
+
+    // Find contours
     vector<vector<Point>> contours;
     findContours(decision,contours,RETR_EXTERNAL,CHAIN_APPROX_NONE);
 
-    // Elimination des anciens objets
-    Objects.clear();
 
-    // Elimination des contours trop long/court
+    // Filter contours
     Rect boundRect;
     for( size_t i = 0; i < contours.size(); i++ ) {
       boundRect = boundingRect( contours[i] );
       int m = (int)sum(mean(decision(boundRect)))[0];
 
+      // If an object fits the parameters
       if ((contours[i].size() > cmin) && (contours[i].size() < cmax) && (m > meanmin)) {
+
+        // Draw the contour of the detected object
         drawContours(display, contours, i, Scalar(255,255,0), 1);
+
+        // Save it
         Objects.push_back({
-          (boundRect.tl() + boundRect.br())/2.0, // Position
+          (boundRect.tl() + boundRect.br())/2.0,
           boundRect
         });
       }
     }
 
-    // Predict tracks
     for (int trck = 0; trck < Tracks.size(); trck++) {
+      // Initialize it to not found
       Tracks[trck].found = false;
-      // Tracks[trck].op = Tracks[trck].p;
-      Tracks[trck].ov = Tracks[trck].v;
 
+      // Predict new position of tracks
+      Tracks[trck].a = Tracks[trck].a;
       Tracks[trck].v = Tracks[trck].v + 1/2*Tracks[trck].a;
       Tracks[trck].p.push_back(Tracks[trck].p.back() + Tracks[trck].v);
+
+      // Draw the new estimated position
       circle(display, Tracks[trck].p.back(), 1, Scalar(0,255,0), 2);
     }
 
@@ -154,42 +169,33 @@ int main(int argc, char* argv[]) {
       int mintrck = -1;
       float dist;
       for (int trck = 0; trck < Tracks.size(); trck++) {
+
         // Measure similarity
         dist = Distance(Objects[obj], Tracks[trck]);
 
+        // Save the one with the minimum distance
         if (dist < mindist) {
           mindist = dist;
           mintrck = trck;
         }
       }
 
-      // Create new track
+      // Create new track if no track match the object
       if ((mindist > distth) || (mintrck == -1)) {
         track NewElem;
         NewElem.p.push_back(Objects[obj].p);
         NewElem.rect = Objects[obj].rect;
         Tracks.push_back(NewElem);
-
-        if (Tracks.size() > 5) {
-          break;
-        }
-
       }
       else {
         // Update track with object
         Tracks[mintrck].kfound++;
         Tracks[mintrck].found = true;
+        Tracks[mintrck].p.back() = Objects[obj].p; // TODO : Bayesian filter
 
-        // Calculate speed and acceleration
-        Point2f dx = Objects[obj].p - Tracks[mintrck].p.end()[-2];
-        Point2f dv = dx;
-        Point2f da = (dv - Tracks[mintrck].ov);
-
-        // Update position, speed and acceleration
-        Tracks[mintrck].p.back() = Objects[obj].p;
-        Tracks[mintrck].v = dv;
-        Tracks[mintrck].a = da;
-
+        // Calculate new speed and acceleration
+        Tracks[mintrck].v = Tracks[mintrck].p.end()[-1] - Tracks[mintrck].p.end()[-2];
+        Tracks[mintrck].a = (Tracks[mintrck].p.end()[-1] - Tracks[mintrck].p.end()[-2]) - (Tracks[mintrck].p.end()[-2] - Tracks[mintrck].p.end()[-3]);
       }
     }
 
@@ -208,7 +214,7 @@ int main(int argc, char* argv[]) {
     // Resample
     vector<track> Tracks2;
     for (int trck = 0; trck < Tracks.size(); trck++) {
-      if (Tracks[trck].klost <= 10 || Tracks[trck].found) {
+      if (Tracks[trck].klost <= klost || Tracks[trck].found) {
         Tracks2.push_back(Tracks[trck]);
       }
     }
@@ -217,7 +223,7 @@ int main(int argc, char* argv[]) {
     // Draw tracks
     for (int trck = 0; trck < Tracks.size(); trck++) {
       circle     (display, Tracks[trck].p.back(), 1, Tracks[trck].color, 2);
-      // arrowedLine(display, Tracks[trck].p.back(), Tracks[trck].p.back() + Tracks[trck].v*10, Tracks[trck].color, 1, 8, 0, 0.1);
+      arrowedLine(display, Tracks[trck].p.back(), Tracks[trck].p.back() + Tracks[trck].v*10, Tracks[trck].color, 1, 8, 0, 0.1);
       // putText    (display, to_string(trck),                Tracks[trck].p.back() + Point2f(0,0),  FONT_HERSHEY_DUPLEX, 0.5, Scalar(255,255,255), 1);
       // putText    (display, to_string(Tracks[trck].kfound), Tracks[trck].p.back() + Point2f(0,10), FONT_HERSHEY_DUPLEX, 0.5, Scalar(255,255,255), 1);
       // putText    (display, to_string(Tracks[trck].klost),  Tracks[trck].p.back() + Point2f(0,20), FONT_HERSHEY_DUPLEX, 0.5, Scalar(255,255,255), 1);
